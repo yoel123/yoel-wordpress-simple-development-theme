e005dd6d641557423c89dccab5c02b307cf4f2f5
yoel123 <yoel12344@walla.co.il>
2016-08-05 00:33:40 +0300

    Update yfuncs.php

--- yfuncs.php
+++ yfuncs.php
@@ -206,13 +206,13 @@ function ypaginate()
 	global $wp_query;
 
 	$big = 999999999; // need an unlikely integer
-
-	echo paginate_links( array(
+	echo "<div class='ypaginate'>".
+	paginate_links( array(
 		'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 		'format' => '?paged=%#%',
 		'current' => max( 1, get_query_var('paged') ),
 		'total' => $wp_query->max_num_pages
-	) );	
+	) )."</div>";	
 }//end ypaginate
 
 /////////////end wp loops//////////////
